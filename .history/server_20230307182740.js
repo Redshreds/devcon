095363ea84675cabebbbb64e83c6a8be632ec7ee
Brandon Bowen <brandonbowen999@gmail.com>
2023-03-20 17:39:17 -0700

    addressing folder

--- .history/server_20230307182740.js
+++
@@ -1,9 +0,0 @@
-const express = require * 'express';
-
-const app = express();
-
-app.get('/', (req, res) => res.send('API Running'));
-
-const PORT = process.env.PORT;
-
-app.lsiten(PORT, () => console.log(`Server started on port ${PORT}`));
--- .history/server_20230307182959.js
+++
@@ -1,9 +0,0 @@
-const express = require * 'express';
-
-const app = express();
-
-app.get('/', (req, res) => res.send('API Running'));
-
-const PORT = process.env.PORT || 5000;
-
-app.lsiten(PORT, () => console.log(`Server started on port ${PORT}`));
