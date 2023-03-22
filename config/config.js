adbc153d5452dc14e60e2debfc118198119424eb
Brandon Bowen <brandonbowen999@gmail.com>
2023-03-21 23:43:03 -0700

    Hail Mary attempt at authentication with Render

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
-const Config = require('config').util.Config;
-const productionConfig = new Config();
+// const Config = require('config').util.Config;
+// const productionConfig = new Config();
+
+const productionConfig = JSON.parse('../PRODUCTION_CONFIG');
 
 module.exports = {
   productionConfig,
