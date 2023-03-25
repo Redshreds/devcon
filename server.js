0058fb4452e8f3ff8a1cd666e366ff9132d8c34c
Brandon Bowen <brandonbowen999@gmail.com>
2023-03-24 22:25:35 -0700

    Edited rollup options in vite build config to include prop-types. Trying a different static serve process.

--- server.js
+++ server.js
@@ -21,11 +21,9 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve Static Assets in Production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/dist'));
-
+  app.use(express.static(path.join(__dirname, 'client', 'dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
